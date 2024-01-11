79f50d9e09ed08b054eb59afa8dec4eb58ba7f5f
kymmsenpai <dzakimaulanapati@gmail.com>
2024-01-11 22:27:18 +0700

    fix little mistake

--- server/routes/teamRouter.go
+++ server/routes/teamRouter.go
@@ -10,5 +10,5 @@ func TeamRouter(incomingRoutes *gin.Engine) {
 
 	// incomingRoutes.POST("/api/v1/team/comment", controller.SendComment)
 	incomingRoutes.GET("/api/v1/team/offers", controller.GetOffers)
-	incomingRoutes.PUT("/api/v1/team/decision", controller.AcceptOffer)
+	incomingRoutes.PUT("/api/v1/team/decision", controller.TeamDecision)
 }
